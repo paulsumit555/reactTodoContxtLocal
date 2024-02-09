8f692c380b42577cb84d59e76966bae437604f3d
paulsumit555 <sumitp.scs@gmail.com>
2024-02-09 17:41:25 -0600

    first commit

---
+++ src/components/TodoContext.js
@@ -0,0 +1,27 @@
+import {createContext, useContext} from 'react'
+
+
+export const TodoContext = createContext({
+
+    todos : [
+        {
+            id:"",
+            todo: "todo message",
+            completed: false
+        }
+    ],
+    addTodo: (todo)=>{},
+    updateTodo: (id,todo)=>{},
+    deleteTodo: (id)=>{},
+    toggleComplete: (id)=>{}
+
+})
+
+
+export const TodoContextProvider = TodoContext.Provider
+
+export const useTodoContext = () => {
+    return useContext(TodoContext)
+}
+
+
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import {TodoContext,TodoContextProvider,useTodoContext} from "./TodoContext"
+
+
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
+
+export {TodoContext,TodoContextProvider,useTodoContext,TodoForm,TodoItem}
